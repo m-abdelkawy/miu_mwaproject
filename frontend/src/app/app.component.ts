c9ac45a92fd55185d59a82ef3ac10ffdc71290b0
amfahadhassan <imfahadhassan@gmail.com>
2021-11-15 16:56:17 -0600

    login & registration frontend

---
+++ frontend/src/app/app.component.ts
@@ -0,0 +1,30 @@
+import { Component } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'demoApp';
+  email: string | any;
+  password: string | any;
+  rname: string | any;
+  remail: string | any;
+  rcpassword: string | any;
+
+  constructor(private snackBar: MatSnackBar) {
+
+  }
+  register() {
+
+  }
+  login() {
+    if (this.email == "admin" && this.password == "admin") {
+      this.snackBar.open('Login Successful', '', { duration: 1000 })
+    } else {
+      this.snackBar.open('Login error', '', { duration: 1000 })
+    }
+  }
+}
