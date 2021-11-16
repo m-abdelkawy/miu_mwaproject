ddecfcb6507409400d5350190dda746db7e32459
amfahadhassan <imfahadhassan@gmail.com>
2021-11-15 22:09:25 -0600

    commit

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -7,7 +7,7 @@ import { MatSnackBar } from '@angular/material/snack-bar';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'demoApp';
+  title = 'appComponent';
   email: string | any;
   password: string | any;
   rname: string | any;
